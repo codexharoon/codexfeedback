caa78131f3b418301ecf421c8531903226ba3571
Muhammad Haroon <theharoonsocial@gmail.com>
2024-06-29 17:14:10 +0500

    route to update accept message status

---
+++ src/app/api/is-accepting-message/route.ts
@@ -0,0 +1,88 @@
+import dbConnection from "@/app/lib/dbConnection";
+import { getServerSession, User } from "next-auth";
+import { authOptions } from "../auth/[...nextauth]/options";
+import USER from "@/app/models/User";
+import { acceptMessageSchema } from "@/app/schemas/acceptMessage";
+
+export async function POST(req: Request) {
+  const session = await getServerSession(authOptions);
+  if (!session || !session.user) {
+    return Response.json(
+      {
+        success: false,
+        message: "Unauthorized",
+      },
+      {
+        status: 401,
+      }
+    );
+  }
+
+  const user: User = session.user as User;
+  await dbConnection();
+
+  try {
+    const { isAcceptingMessage } = await req.json();
+
+    const validateIsAccMsg = acceptMessageSchema.safeParse({
+      acceptMessage: isAcceptingMessage,
+    });
+
+    if (!validateIsAccMsg.success) {
+      return Response.json(
+        {
+          success: false,
+          message:
+            validateIsAccMsg.error.format().acceptMessage?._errors[0] ||
+            "Invalid data provided (required boolean)",
+        },
+        {
+          status: 400,
+        }
+      );
+    }
+
+    const updatedUser = await USER.findByIdAndUpdate(
+      user?._id,
+      {
+        isAcceptingMessage,
+      },
+      {
+        new: true,
+      }
+    );
+
+    if (!updatedUser) {
+      return Response.json(
+        {
+          success: false,
+          message: "User not found",
+        },
+        {
+          status: 404,
+        }
+      );
+    }
+
+    return Response.json(
+      {
+        success: true,
+        message: "User accept message status updated successfully",
+      },
+      {
+        status: 200,
+      }
+    );
+  } catch (error) {
+    console.log("error in is-accepting-message route: ", error);
+    return Response.json(
+      {
+        success: false,
+        message: "error in is-accepting-message route: ",
+      },
+      {
+        status: 500,
+      }
+    );
+  }
+}
