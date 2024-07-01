bea79ceac96945640dc14f8a2fee14b81068b1ab
Muhammad Haroon <theharoonsocial@gmail.com>
2024-07-01 18:17:31 +0500

    fix bugs + completed signup in frontend

--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -4,53 +4,67 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
 import { userSignupSchema } from "@/app/schemas/userSignup";
-import { useDebounceValue } from "usehooks-ts";
+import { useDebounceCallback } from "usehooks-ts";
 import { useEffect, useState } from "react";
 import axios, { AxiosError } from "axios";
 import { ApiResponse } from "@/app/types/ApiResponse";
 import { useToast } from "@/components/ui/use-toast";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { Loader2 } from "lucide-react";
+import { Span } from "next/dist/trace";
 
 const Signup = () => {
-  const [username, setUsername] = useState<string>("haroon");
+  const [username, setUsername] = useState<string>("");
   const [usernameErrMsg, setUsernameErrMsg] = useState<string>("");
   const [findUsernameLoading, setFindUsernameLoading] =
     useState<boolean>(false);
 
   const [formSubmitLoading, setFormSubmitLoading] = useState<boolean>(false);
 
-  const [debouncedUsername, setDebouncedUsername] = useDebounceValue(
-    username,
-    300
-  );
+  const debounced = useDebounceCallback(setUsername, 500);
 
   const { toast } = useToast();
   const router = useRouter();
 
   useEffect(() => {
     const checkUsername = async () => {
-      setUsernameErrMsg("");
-      setFindUsernameLoading(true);
-      try {
-        const response = await axios.get(
-          `/api/check-username/?username=${debouncedUsername}`
-        );
-        console.log(response);
+      if (username) {
+        setUsernameErrMsg("");
+        setFindUsernameLoading(true);
+        try {
+          const response = await axios.get(
+            `/api/check-username/?username=${username}`
+          );
+          console.log(response);
 
-        setUsernameErrMsg(response.data.message);
-      } catch (error) {
-        const axiosErr = error as AxiosError<ApiResponse>;
-        setUsernameErrMsg(
-          axiosErr.response?.data.message || "An error occurred"
-        );
-      } finally {
-        setFindUsernameLoading(false);
+          const dataMsg = response.data.message;
+          setUsernameErrMsg(dataMsg);
+        } catch (error) {
+          const axiosErr = error as AxiosError<ApiResponse>;
+          setUsernameErrMsg(
+            axiosErr.response?.data.message || "An error occurred"
+          );
+        } finally {
+          setFindUsernameLoading(false);
+        }
+      } else {
+        setUsernameErrMsg("");
       }
     };
 
     checkUsername();
-  }, []);
+  }, [username]);
 
   const form = useForm<z.infer<typeof userSignupSchema>>({
     resolver: zodResolver(userSignupSchema),
@@ -93,7 +107,107 @@ const Signup = () => {
     }
   }
 
-  return <div>Signup</div>;
+  return (
+    <div className="flex justify-center items-center min-h-screen">
+      <div className="border p-10 w-[30%]">
+        <div className="text-center mb-3">
+          <h1 className="text-lg font-bold">Signup to CodexFeedback</h1>
+          <p className="text-xs">It takes less then a minute.</p>
+        </div>
+        <div className="w-full">
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+              <FormField
+                control={form.control}
+                name="username"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Username</FormLabel>
+                    <FormControl>
+                      <Input
+                        placeholder="username"
+                        {...field}
+                        onChange={(e) => {
+                          field.onChange(e);
+                          debounced(e.target.value);
+                        }}
+                      />
+                    </FormControl>
+
+                    {
+                      <p className="text-xs">
+                        {findUsernameLoading ? (
+                          <Loader2 className="h-4 w-4 animate-spin" />
+                        ) : usernameErrMsg === "Username is available" ? (
+                          <span className="text-green-500">
+                            {usernameErrMsg}
+                          </span>
+                        ) : (
+                          <span className="text-red-500">{usernameErrMsg}</span>
+                        )}
+                      </p>
+                    }
+
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Email</FormLabel>
+                    <FormControl>
+                      <Input type="email" placeholder="email" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="password"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Password</FormLabel>
+                    <FormControl>
+                      <Input
+                        type="password"
+                        placeholder="password"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <Button type="submit" disabled={formSubmitLoading}>
+                {formSubmitLoading ? (
+                  <span className="flex gap-2 justify-center items-center">
+                    <Loader2 className="h-4 w-4 animate-spin" />{" "}
+                    <span>Please Wait</span>
+                  </span>
+                ) : (
+                  "Submit"
+                )}
+              </Button>
+            </form>
+          </Form>
+        </div>
+
+        <div className="text-center mt-5">
+          <p className="text-xs">
+            Already have an account?{" "}
+            <Link href="/signin">
+              {" "}
+              <span className="text-blue-400">Login</span>
+            </Link>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Signup;
