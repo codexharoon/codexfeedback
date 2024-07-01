196c444e45ba55f57de54763a2252d43e67306bf
Muhammad Haroon <theharoonsocial@gmail.com>
2024-07-01 23:30:38 +0500

    improve zod login schema

--- src/app/schemas/userLogin.ts
+++ src/app/schemas/userLogin.ts
@@ -1,6 +1,10 @@
 import { z } from "zod";
 
 export const userLoginSchema = z.object({
-  identifier: z.string(),
-  password: z.string(),
+  identifier: z
+    .string()
+    .min(3, { message: "Please enter a valid identifier." }),
+  password: z
+    .string()
+    .min(6, { message: "Password must be at least 6 character long" }),
 });
