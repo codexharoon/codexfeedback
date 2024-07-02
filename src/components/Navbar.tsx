55650ee86d5b613007a14a5eb0ca4c9f24599a38
Muhammad Haroon <theharoonsocial@gmail.com>
2024-07-02 17:06:02 +0500

    created navbar component with user session nextauth

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import { useSession, signOut } from "next-auth/react";
+import { User } from "next-auth";
+import { Button } from "./ui/button";
+import { useRouter } from "next/navigation";
+
+const Navbar = () => {
+  const { data: session, status } = useSession();
+
+  const user: User = session?.user as User;
+
+  const router = useRouter();
+
+  return (
+    <nav className="bg-slate-200 p-5">
+      {status === "authenticated" ? (
+        <div className="flex justify-between items-center">
+          <span className="text-lg font-bold">
+            Welcome, {session.user.username || session.user.email}
+          </span>
+          <Button onClick={() => signOut()}>Signout</Button>
+        </div>
+      ) : (
+        <div className="flex justify-between items-center">
+          <span className="text-lg font-bold">CodexFeedback</span>
+          <Button onClick={() => router.push("/signin")}>Login</Button>
+        </div>
+      )}
+    </nav>
+  );
+};
+
+export default Navbar;
